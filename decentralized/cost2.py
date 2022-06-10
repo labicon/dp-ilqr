9cb48b912df06d996ef7b2de3b3cdeb23387cfef
Zach Williams <zjwilliams20@gmail.com>
2022-06-10 00:28:14 -0500

    Reverted back to using torch only and added other dynamical models

--- decentralized/cost2.py
+++ decentralized/cost2.py
@@ -61,6 +61,65 @@ class AutoDiffCost(Cost):
         return L_x, L_u, L_xx, L_uu, L_ux
 
 
+class ReferenceCost(AutoDiffCost):
+    """
+    The cost of a state and control from some reference trajectory.
+    """
+
+    def __init__(self, xf, Q, R, Qf=None):
+
+        if Qf is None:
+            Qf = torch.eye(Q.shape[0])
+
+        # Define states as rows so that xf doesn't broadcast x in __call__.
+        self.xf = xf.reshape(1, -1)
+
+        self.Q = Q
+        self.R = R
+        self.Qf = Qf
+
+        self.Q_plus_QT = self.Q + self.Q.T
+        self.R_plus_RT = self.R + self.R.T
+
+    def __call__(self, x, u, terminal=False):
+        if not terminal:
+            return (x - self.xf) @ self.Q @ (x - self.xf).T + u @ self.R @ u.T
+        return (x - self.xf) @ self.Qf @ (x - self.xf).T
+
+
+class ProximityCost(AutoDiffCost):
+    def __init__(self, x_dims, radius):
+        self.x_dims = x_dims
+        self.radius = radius
+
+    def __call__(self, x):
+        """Penalizes distances underneath some radius between agents"""
+        distances = compute_pairwise_distance(x, self.x_dims)
+        pair_costs = torch.fmin(torch.zeros((1)), distances - self.radius) ** 2
+        return pair_costs.sum(dim=0)
+
+
+class GameCost(AutoDiffCost):
+    def __init__(self, reference_cost, proximity_cost=None):
+
+        if not proximity_cost:
+
+            def proximity_cost(*_):
+                return 0.0
+
+        self.ref_cost = reference_cost
+        self.prox_cost = proximity_cost
+
+        self.REF_WEIGHT = 1.0
+        self.PROX_WEIGHT = 100.0
+
+    def __call__(self, x, u, terminal=False):
+        return (
+            self.PROX_WEIGHT * self.prox_cost(x)
+            + self.REF_WEIGHT * self.ref_cost(x, u, terminal=terminal)[0]
+        )
+
+
 class NumericalDiffCost(Cost):
 
     """Finite difference quadraticized cost
@@ -107,65 +166,3 @@ class NumericalDiffCost(Cost):
         )
 
         return Lx(x, u), Lu(x, u), L_xx, L_uu, L_ux
-
-
-class ReferenceCost(AutoDiffCost):
-    """
-    The cost of a state and control from some reference trajectory.
-    """
-
-    def __init__(self, xf, Q, R, Qf=None):
-        mm = torch if torch.is_tensor(xf) else np
-        if Qf is None:
-            Qf = mm.eye(Q.shape[0])
-
-        # Define states as rows so that xf doesn't broadcast x in __call__.
-        self.xf = xf.reshape(1, -1)
-
-        self.Q = Q
-        self.R = R
-        self.Qf = Qf
-
-        self.Q_plus_QT = self.Q + self.Q.T
-        self.R_plus_RT = self.R + self.R.T
-
-    def __call__(self, x, u, terminal=False):
-        if not terminal:
-            return (x - self.xf) @ self.Q @ (x - self.xf).T + u @ self.R @ u.T
-        return (x - self.xf) @ self.Qf @ (x - self.xf).T
-
-
-class ProximityCost(AutoDiffCost):
-    def __init__(self, x_dims, radius):
-        self.x_dims = x_dims
-        self.radius = radius
-
-    def __call__(self, x):
-        """Penalizes distances underneath some radius between agents"""
-        distances = compute_pairwise_distance(x, self.x_dims)
-        if torch.is_tensor(x):
-            pair_costs = torch.fmin(torch.zeros((1)), distances - self.radius) ** 2
-            return pair_costs.sum(dim=0)
-        pair_costs = np.fmin(np.zeros((1)), distances - self.radius) ** 2
-        return pair_costs.sum(axis=0)
-
-
-class GameCost(AutoDiffCost):
-    def __init__(self, reference_cost, proximity_cost=None):
-
-        if not proximity_cost:
-
-            def proximity_cost(*_):
-                return 0.0
-
-        self.ref_cost = reference_cost
-        self.prox_cost = proximity_cost
-
-        self.REF_WEIGHT = 1.0
-        self.PROX_WEIGHT = 100.0
-
-    def __call__(self, x, u, terminal=False):
-        return (
-            self.PROX_WEIGHT * self.prox_cost(x)
-            + self.REF_WEIGHT * self.ref_cost(x, u, terminal=terminal)[0]
-        )
--- decentralized/dynamics2.py
+++ decentralized/dynamics2.py
@@ -6,6 +6,7 @@ import abc
 
 import numpy as np
 from scipy.linalg import block_diag
+from scipy.optimize import approx_fprime
 import torch
 
 from .util import split_agents
@@ -19,45 +20,24 @@ class DynamicalModel(abc.ABC):
         self.n_u = n_u
         self.dt = dt
 
+        self.NX_EYE = np.eye(self.n_x, dtype=np.float32)
+
     def __call__(self, x, u):
         """Zero-order hold to integrate continuous dynamics f"""
         return x + self.f(x, u) * self.dt
 
+    @staticmethod
     @abc.abstractmethod
     def f():
         """Continuous derivative of dynamics with respect to time"""
         pass
 
-    @abc.abstractmethod
-    def linearize():
-        """Jacobian linearization with respect to states and controls"""
-        pass
-
-
-class AutoDiffModel(DynamicalModel):
-    """Mix-in to use torch automatic differentiation for linearization"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.NX_EYE = np.eye(self.n_x, dtype=np.float32)
-
-    def linearize(self, x, u, discrete=False):
+    def linearize(self, x: torch.tensor, u: torch.tensor, discrete=False):
         """Compute the Jacobian linearization of the dynamics for a particular state
            and controls for all players.
         """
 
-        assert torch.is_tensor(x) == torch.is_tensor(u)
-        convert_to_torch = not torch.is_tensor(x) and not torch.is_tensor(u)
-
-        if convert_to_torch:
-            x = torch.from_numpy(x).requires_grad_(True)
-            u = torch.from_numpy(u).requires_grad_(True)
-
         A, B = torch.autograd.functional.jacobian(self.f, (x, u))
-
-        if convert_to_torch:
-            A = A.detach().numpy()
-            B = B.detach().numpy()
 
         if discrete:
             return A, B
@@ -68,7 +48,7 @@ class AutoDiffModel(DynamicalModel):
         return A, B
 
 
-class MultiDynamicalModel(AutoDiffModel):
+class MultiDynamicalModel(DynamicalModel):
     """Encompasses the dynamical simulation and linearization for a collection of
        DynamicalModel's
     """
@@ -84,30 +64,92 @@ class MultiDynamicalModel(AutoDiffModel):
 
     def f(self, x, u):
         """Integrate the dynamics for the combined decoupled dynamical model"""
-        mm = torch if torch.is_tensor(x) else np
-        return mm.stack(
+        return torch.cat(
             [
                 submodel.f(xi.flatten(), ui.flatten())
                 for submodel, xi, ui in zip(self.submodels, *self.partition(x, u))
             ]
-        ).flatten()
+        )
 
     def partition(self, x, u):
         """Helper to split up the states and control for each subsystem"""
         return split_agents(x, self.x_dims), split_agents(u, self.u_dims)
 
-    def _linearize_arbitary(self, x, u):
-        """Compute the submodel-linearizations (deprecated)
-           NOTE: This function is an alternate way to do the linearization rather than
-           using the AutoDiffModel mix-in.
-        """
 
-        sub_linearizations = [
-            submodel.linearize(xi.flatten(), ui.flatten())
-            for submodel, xi, ui in zip(self.submodels, *self.partition(x, u))
-        ]
+class DoubleIntDynamics4D(DynamicalModel):
+    def __init__(self, dt):
+        super().__init__(4, 2, dt)
 
-        sub_As = [AB[0] for AB in sub_linearizations]
-        sub_Bs = [AB[1] for AB in sub_linearizations]
+    @staticmethod
+    def f(x, u):
+        *_, vx, vy = x
+        ax, ay = u
+        return torch.stack([vx, vy, ax, ay])
 
-        return block_diag(*sub_As), block_diag(*sub_Bs)
+
+class CarDynamics3D(DynamicalModel):
+    def __init__(self, dt):
+        super().__init__(3, 2, dt)
+
+    @staticmethod
+    def f(x, u):
+        *_, theta = x
+        v, omega = u
+        return torch.stack([v * torch.cos(theta), v * torch.sin(theta), omega])
+
+
+class UnicycleDynamics4D(DynamicalModel):
+    def __init__(self, dt):
+        super().__init__(4, 2, dt)
+
+    @staticmethod
+    def f(x, u):
+        *_, theta, v = x
+        a, omega = u
+        return torch.stack([v * torch.cos(theta), v * torch.sin(theta), a, omega])
+
+
+class BikeDynamics5D(DynamicalModel):
+    def __init__(self, dt):
+        super().__init__(5, 2, dt)
+
+    @staticmethod
+    def f(x, u):
+        *_, theta, v, phi = x
+        a, phi_dot = u
+        return torch.stack(
+            [v * torch.cos(theta), v * torch.sin(theta), torch.tan(phi), a, phi_dot]
+        )
+
+
+def linearize_finite_difference(f, x, u):
+    """"Linearization using finite difference.
+
+    REF: https://github.com/anassinator/ilqr/blob/master/ilqr/dynamics.py
+    NOTE: deprecated in favor of automatic differentiation.
+    """
+
+    n_x = x.size
+    jac_eps = np.sqrt(np.finfo(float).eps)
+
+    A = np.vstack([approx_fprime(x, lambda x: f(x, u)[i], jac_eps) for i in range(n_x)])
+    B = np.vstack([approx_fprime(u, lambda u: f(x, u)[i], jac_eps) for i in range(n_x)])
+
+    return A, B
+
+
+def linearize_multi(submodels, partition, x, u):
+    """Compute the submodel-linearizations
+
+    NOTE: deprecated in favor of automatic differentiation.
+    """
+
+    sub_linearizations = [
+        submodel.linearize(xi.flatten(), ui.flatten())
+        for submodel, xi, ui in zip(submodels, *partition(x, u))
+    ]
+
+    sub_As = [AB[0] for AB in sub_linearizations]
+    sub_Bs = [AB[1] for AB in sub_linearizations]
+
+    return block_diag(*sub_As), block_diag(*sub_Bs)
