6191725e6c2a99f035930bd5476024a490fc7964
Zach Williams <zjwilliams20@gmail.com>
2022-09-07 14:34:40 -0500

    updated proximity cost

--- decentralized/cost.py
+++ decentralized/cost.py
@@ -183,7 +183,7 @@ class GameCost(Cost):
         self.prox_cost = proximity_cost
 
         self.REF_WEIGHT = 1.0
-        self.PROX_WEIGHT = 200.0
+        self.PROX_WEIGHT = 1000.0
 
         self.x_dims = [ref_cost.x_dim for ref_cost in self.ref_costs]
         self.u_dims = [ref_cost.u_dim for ref_cost in self.ref_costs]
