5fcaa19c95529eddd027893919580787c289bffc
RandyChen <jushanc2@illinois.edu>
2022-10-24 16:03:32 -0500

    type error found in sim_drone_main

--- run/do_mpc/sim_drone_main.py
+++ run/do_mpc/sim_drone_main.py
@@ -18,10 +18,12 @@ n_inputs = 3
 
 def setup_baseline(x_baseline, x_baseline_f, v_max, theta_max, phi_max, tau_max,\
                     x_dims, u_dims, Q, R, Qf, n_agents, n_dims, radius):
-    model_baseline = [baseline_drone_model(dec.split_agents(x_baseline_f.reshape(1,-1),x_dims)[i].flatten(), Q, R, Qf, x_baseline) for i in range(n_agents)] 
+    model_baseline = [baseline_drone_model(dec.split_agents(x_baseline_f.reshape(1,-1),x_dims)[i].flatten(), Q, \
+                     R, Qf, x_baseline, x_dims) for i in range(n_agents)] 
     #a list of baseline models for each agent
     
-    mpc_baseline = [baseline_drone_mpc(model_i,n_agents,x_baseline1,x_dims, v_max, theta_max, phi_max, tau_max) for model_i in model_baseline]
+    mpc_baseline = [baseline_drone_mpc(model_i,n_agents,x_baseline, x_dims, v_max, theta_max, phi_max,\
+                   tau_max) for model_i in model_baseline]
     #a list of baseline mpc controllers for each agent
     
     simulator_baseline = [baseline_drone_simulator(model_i) for model_i in model_baseline]
@@ -45,8 +47,10 @@ def setup_baseline(x_baseline, x_baseline_f, v_max, theta_max, phi_max, tau_max,
         mpc_baseline[m].set_initial_guess()
         u0_baseline.append(mpc_baseline[m].make_step(split_states[m].T))
         x_baseline_next.append(simulator_baseline[m].make_step(split_inputs[m].T))
-    # print(mpc_baseline.data._lam_g_num)
-    return u0_baseline, x_baseline_next, [mpc_baseline[i].data._lam_g_num for i in range(len(mpc_baseline))]
+
+    return [u0_baseline, x_baseline_next, [mpc_baseline[i].data._lam_g_num for i in range(len(mpc_baseline))]] 
+    #last _lam_g_num is the Lagrange multiplier
+    
 
 def run_sim():
     
@@ -56,6 +60,7 @@ def run_sim():
     v_max = 5
 
     x_dims = [n_states]*n_agents
+    u_dims = [n_inputs]*n_agents
 
     Q = np.eye(n_states)*10
     Qf = np.eye(n_states)*1e3
@@ -63,12 +68,12 @@ def run_sim():
 
     radius = 0.5
     n_dims = [3,3,3]
-    episode=200
+    episode= 200
     x_baseline_init, x_baseline_f = util.paper_setup_3_quads()
 
-    x_baseline1 = x_baseline_init
+    x_baseline1 = x_baseline_init #concatenated states of all agents
 
-    states_list = np.zeros((episode+1,9)) #positions of each drone
+    states_list = np.zeros((episode+1,3*n_agents)) #positions of each drone
     states_list[0,:] = np.array([x_baseline1[0],x_baseline1[1],x_baseline1[2],\
                             x_baseline1[6],x_baseline1[7],x_baseline1[8],\
                             x_baseline1[12],x_baseline1[13],x_baseline1[14]]).flatten()
@@ -85,9 +90,11 @@ def run_sim():
         for k in range(episode):
     
             f = executor.submit(setup_baseline(x_baseline1, x_baseline_f, v_max, theta_max, phi_max, tau_max,\
-                    x_dims, Q, R, Qf, n_agents, n_dims, radius), x_baseline1, x_baseline_f, 1.8)
+                    x_dims, u_dims, Q, R, Qf, n_agents, n_dims, radius), x_baseline1, x_baseline_f, 1.8)
 
-            _, x_baseline1, la_mul = f.result()
+            # _, x_baseline1, la_mul = f.result()
+            x_baseline1 = f.result()[1]
+            la_mul = f.result()[2]
 
             print("Lagrange Multiplier: ", la_mul)
             print("Length of Lagrange Multiplier: ", len(la_mul[0]))
