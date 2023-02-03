bc08a1c5bdaba40cd9cc6a3834855c4e09349878
Zach Williams <zjwilliams20@gmail.com>
2023-02-02 19:58:20 -0600

    Revampled project tooling with editable install via setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name="Barebones Dynamics Library",
-    ext_modules=cythonize("dpilqr/bbdynamicswrap.pyx"),
+    name="bbdynamics",
+    ext_modules=cythonize(
+        "dpilqr/bbdynamicswrap.pyx",
+        compiler_directives={"language_level": "3"},
+    ),
     zip_safe=False,
 )
