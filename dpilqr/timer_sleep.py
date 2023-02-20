dd919b53f3dbe0259c4ec418d14118a7b9491fc4
randychen <jushanc2@illinois.edu>
2023-02-19 21:04:36 -0600

    removed sleep function

--- dpilqr/timer_sleep.py
+++ dpilqr/timer_sleep.py
@@ -1,31 +1,22 @@
 import threading
-from time import perf_counter
+
 import rclpy
-import time
 
+rclpy.init()
+node = rclpy.create_node('simple_node')
 
-def set_sleep_rate(frequency):
-    rclpy.init()
-    node = rclpy.create_node('simple_node')
+# Spin in a separate thread
+thread = threading.Thread(target=rclpy.spin, args=(node, ), daemon=True)
+thread.start()
 
-    # Spin in a separate thread
-    thread = threading.Thread(target=rclpy.spin, args=(node, ), daemon=True)
-    thread.start()
+rate = node.create_rate(2)
 
-    rate = node.create_rate(frequency)
-    return rate
+try:
+    while rclpy.ok():
+        print('Help me body, you are my only hope')
+        rate.sleep()
+except KeyboardInterrupt:
+    pass
 
-    # try:
-    #     while rclpy.ok():
-    #         # time.sleep(0.6)
-    #         # print('Help me body, you are my only hope')
-    #         t0 = perf_counter()
-    #         rate.sleep()
-    #         tf = perf_counter()
-    #         # print(f'{(tf-t0)} seconds passed')
-    # except KeyboardInterrupt:
-    #     pass
-
-    # rclpy.shutdown()
-    # thread.join()
-    
+rclpy.shutdown()
+thread.join()
